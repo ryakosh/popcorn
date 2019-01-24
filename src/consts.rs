554420485b05813079c68dab28c03ab9dea1a029
ryakosh <ryakosh@gmail.com>
2019-01-24 16:27:05 +0330

    Add consts module

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,7 @@
 use crate::regex::Regex;
 
 lazy_static! {
-  static ref RGX_EMAIL: Regex = Regex::new(r"^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$").expect("Invalid email regex");
-  static ref RGX_UNAME: Regex = Regex::new(r"^[a-z0-9_-]+$").expect("Invalid username regex");
-  static ref RGX_PWD: Regex = Regex::new(r"^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$").expect("Invalid password regex");
+  pub static ref RGX_EMAIL: Regex = Regex::new(r"^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$").expect("Invalid email regex");
+  pub static ref RGX_UNAME: Regex = Regex::new(r"^[a-z0-9_-]+$").expect("Invalid username regex");
+  pub static ref RGX_PWD: Regex = Regex::new(r"^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$").expect("Invalid password regex");
 }
