28be004c99c87b695efadce9074529312e5e3581
ryakosh <ryakosh@gmail.com>
2019-01-24 16:30:58 +0330

    Add lazy_static, regex crates and define types, consts modules

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,11 @@ mod tests {
 }
 
 #[macro_use] extern crate diesel;
+#[macro_use] extern crate lazy_static;
 extern crate chrono;
+extern crate regex;
 
 pub mod db;
 pub mod error;
+pub mod types;
+pub mod consts;
