754241b2f5c2f2242f610d909185b3860116c786
ryakosh <ryakosh@gmail.com>
2019-01-24 19:55:12 +0330

    Add connect, signup functions

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,35 @@
-use crate::error::Error;
-
 mod schema;
 mod models;
+
+use std::env::var;
+use crate::diesel::*;
+use crate::types::CandidateUser;
+use crate::error::*;
+use schema::users::dsl::*;
+use models::{User};
+
+fn connect(conn_str: &str) -> PgConnection {
+  PgConnection::establish(conn_str)
+    .expect(&format!("Error connecting to: {}", conn_str))
+}
+
+pub fn signup(candidate: &CandidateUser) -> Result<User, Errors> {
+  let conn = connect(&var("DATABASE_URL")
+    .expect("Can't find DATABASE_URL environment variable"));
+  let user: Result<User, _> = users.find(candidate.uname().to_lowercase()).first(&conn);
+
+  if let Ok(_) = user {
+    Err(vec![Error::UnameTaken])
+  } else {
+    let new_user = User {
+      id: candidate.uname().to_string(),
+      email: candidate.email().to_string(),
+      pwd: candidate.pwd().to_string(),
+    };
+
+    Ok(insert_into(users)
+      .values(&new_user)
+      .get_result(&conn)
+      .expect("Error creating a user"))
+  }
+}
