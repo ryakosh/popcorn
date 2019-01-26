efa66f255d9b055ed2cc7758947a0deb2cf17eeb
ryakosh <ryakosh@gmail.com>
2019-01-26 16:17:53 +0330

    Add signin function

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,8 +3,10 @@ mod models;
 
 use std::env::var;
 use crate::diesel::*;
-use crate::types::data::SignupData;
+use crate::types::Claims;
+use crate::types::data::{SignupData, SigninData};
 use crate::error::*;
+use crate::jsonwebtoken::{encode, Header};
 use schema::users::dsl::*;
 use models::{User};
 
@@ -31,5 +33,20 @@ pub fn signup(singup_data: &SignupData) -> Result<User, Errors> {
       .values(&new_user)
       .get_result(&conn)
       .expect("Error creating a user"))
+  }
+}
+
+pub fn signin(signin_data: &SigninData) -> Result<String, Errors> {
+  let conn = connect(&var("DATABASE_URL")
+    .expect("Can't find DATABASE_URL environment variable"));
+  let result: Result<User, _> = 
+    users.find(signin_data.uname().to_lowercase()).first(&conn);
+
+  if let Ok(user) = result {
+    // TODO: Change secret to something more secure
+    Ok(encode(&Header::default(), &Claims::new(user.id), "secret".as_ref())
+      .expect("Error encoding token"))
+  } else {
+    Err(vec![Error::UserNFound])
   }
 }
