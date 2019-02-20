4a534b8f20a36ea1469e485a4e905fc25d2c91f0
ryakosh <ryakosh@gmail.com>
2019-02-20 17:53:38 +0330

    Add Genres struct

--- src/filter/types.rs
+++ src/filter/types.rs
@@ -1,17 +1,34 @@
 use crate::error::Error;
+use crate::consts::RGX_ALPHA;
 
-pub struct ReleaseCountry<'r> {
-  release_country: &'r str,
+pub struct ReleaseCountry {
+  release_country: String,
 }
 
-impl<'r> ReleaseCountry<'r> {
-  fn new(release_country: &'r str) -> Result<Self, Error> {
+impl ReleaseCountry {
+  pub fn new(release_country: &str) -> Result<Self, Error> {
     if release_country.len() == 2 {
       Ok(ReleaseCountry {
         release_country: release_country.to_uppercase(),
       })
     } else {
-      Err(Error::InvalidFilter)
+      Err(Error::FilterInvalid)
+    }
+  }
+}
+
+pub struct Genres<'g> {
+  genres: Vec<&'g str>,
+}
+
+impl<'g> Genres<'g> {
+  pub fn new(genres: &'g str) -> Result<Self, Error> {
+    let genres: Vec<&'g str> = genres.split("|").collect();
+
+    if genres.iter().all(|genre| RGX_ALPHA.is_match(genre)) {
+      Ok(Genres { genres })
+    } else {
+      Err(Error::FilterInvalid)
     }
   }
 }
