691e924a70c6ce965dc1547534126edd3dd3c72b
ryakosh <ryakosh@gmail.com>
2019-02-25 15:12:17 +0330

    Add &self to Filter

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -3,12 +3,6 @@ pub mod types;
 use std::marker::PhantomData;
 use crate::error::Error;
 
-pub enum Object<'o, P, F: Filter<'o>> {
-  Type,
-  Instance(F),
-  Phantom(PhantomData<&'o P>)
-}
-
 pub trait Filter<'f> {
-  fn new(filter: &'f str) -> Result<Box<Self>, Error>;
+  fn new(&self, filter: &'f str) -> Result<Box<Self>, Error>;
 }
