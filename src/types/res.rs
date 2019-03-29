f220a534869bb1746fa9d90afc73dc1294afd8bd
ryakosh <ryakosh@gmail.com>
2019-03-29 15:57:48 +0430

    Add MovieRes struct

--- src/types/res.rs
+++ src/types/res.rs
@@ -1,4 +1,23 @@
+use crate::db::models::{Writer, Director, Artist};
+
 #[derive(Serialize)]
 pub struct SigninRes {
   pub token: String,
+}
+
+#[drive(Serialize)]
+pub struct MovieRes {
+  movie_id: i32,
+  title: String,
+  description: String,
+  poster: String,
+  genres: Vec<String>,
+  languages: Vec<String>,
+  release_country: String,
+  #[serde(skip)]
+  release_date: NaiveDate,
+  duration: i16,
+  writers: Vec<Writer>,
+  directors: Vec<Director>,
+  stars: Vec<Artist>,
 }
