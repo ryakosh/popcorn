9fb06bb5e8e8dc8412b0ba261342ba007648c07a
ryakosh <ryakosh@gmail.com>
2020-02-16 18:31:27 +0330

    Use argon2 hashing algorithm for user passwords

--- src/db/auth.rs
+++ src/db/auth.rs
@@ -1,11 +1,12 @@
 use super::{connect, models::User, schema::users};
-use crate::config::CONFIG;
+use crate::argon2;
+use crate::config::{self, CONFIG};
 use crate::diesel::{self, prelude::*};
 use crate::error::Error;
+use crate::getrandom;
 use crate::jsonwebtoken::{encode, Header};
 use crate::types::{
     data::{SigninData, SignupData},
-    req_guards::ClaimedUser,
     Claims,
 };
 use std::env::var;
@@ -18,13 +19,13 @@ pub fn signup(signup_data: &SignupData) -> Result<(), Error> {
         .find(signup_data.uname().to_lowercase())
         .first(&conn);
 
-    if let Ok(_) = user {
+    if user.is_ok() {
         Err(Error::UnameTaken)
     } else {
         let new_user = User {
             id: signup_data.uname().to_string(),
             email: signup_data.email().to_string(),
-            pwd: signup_data.pwd().to_string(),
+            pwd: gen_pwd_hash(signup_data.pwd(), &CONFIG.argon2),
         };
 
         diesel::insert_into(users::table)
@@ -45,7 +46,7 @@ pub fn signin(signin_data: &SigninData) -> Result<String, Error> {
         .first(&conn);
 
     if let Ok(user) = result {
-        if signin_data.pwd() == user.pwd {
+        if verify_pwd(signin_data.pwd(), &user.pwd) {
             Ok(encode(
                 &Header::default(),
                 &Claims::new(user.id),
@@ -68,4 +69,47 @@ pub fn get_user_id(uname: &str) -> Result<String, Error> {
         .select(users::id)
         .get_result(&conn)
         .map_err(|_| Error::UserNFound)
+}
+
+fn gen_pwd_hash(pwd: &str, config_argon2: &config::Argon2) -> String {
+    let mut salt = vec![0u8; config_argon2.salt_length];
+    let argon2_config = argon2::Config {
+        variant: argon2::Variant::Argon2i,
+        version: argon2::Version::Version13,
+        mem_cost: config_argon2.mem_cost,
+        time_cost: config_argon2.time_cost,
+        lanes: config_argon2.lanes,
+        thread_mode: argon2::ThreadMode::Parallel,
+        hash_length: config_argon2.hash_length,
+        ..argon2::Config::default()
+    };
+
+    getrandom::getrandom(&mut salt).expect("Error generating random salt");
+    argon2::hash_encoded(pwd.as_bytes(), &salt, &argon2_config)
+        .expect("Error generating password hash")
+}
+
+fn verify_pwd(pwd: &str, hash: &str) -> bool {
+    argon2::verify_encoded(hash, pwd.as_bytes()).expect("Error verifying password")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pwd_hash_generation_works_correctly() {
+        let test_pwd = "test_pwd123";
+        let test_config = config::Argon2 {
+            mem_cost: 100,
+            time_cost: 1,
+            lanes: 1,
+            salt_length: 16,
+            hash_length: 16,
+        };
+
+        let hash = gen_pwd_hash(test_pwd, &test_config);
+
+        assert!(verify_pwd(test_pwd, &hash));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ extern crate lazy_static;
 extern crate serde_derive;
 #[macro_use]
 extern crate rocket;
+extern crate argon2;
 extern crate chrono;
+extern crate getrandom;
 extern crate jsonwebtoken;
 extern crate regex;
 extern crate serde;
